339b01045bdcc6fc40bbe8c1b1f95f78d74a52e7
Abbas S <s.4bbas@gmail.com>
2023-04-22 05:25:19 +0000

    Started project.

---
+++ src/libWad/Wad.h
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+struct Descriptor
+{
+public:
+    uint32_t eOffset;
+    uint32_t length;
+    string name;
+    string filePath;
+};
+
+class Wad
+{
+private:
+public:
+    static Wad *loadWad(const string &path);
+    string getMagic();
+    bool isContent(const string &path);
+    bool isDirectory(const string &path);
+    int getSize(const string &path);
+    int getContents(const string &path, char *buffer, int length, int offset = 0);
+    int getDirectory(const string &path, vector<string> *directory);
+};
